9135e3ac2d8157e9e57bb10fcce85c6744530978
Gold-Berg <igbinobaeseosa0@gmail.com>
2023-06-25 17:48:20 -0700

    #

--- manager.c
+++ manager.c
@@ -47,6 +47,8 @@ int run_opcodes(char **cmds)
 			divde(line_number);
 		else if (strcmp(temp, "mod") == 0)
 			mod(line_number);
+		else if (strcmp(temp, "#") == 0)
+			nop(line_number);
 		else
 		{
 			fprintf(stderr, "L%i: unknown instruction %s\n", line_number, temp);
